eb05ba831528dfd90d9cfa04cbde0889f3b26202
cleberPSA <cleberpacheco.araujo@gmail.com>
2023-10-26 11:46:52 -0300

    Testando a Lógica do programa, e instanciando as classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia(numero: "123456", modelo: "Nokia", imei: "2222", memoria: 2048);
+Console.WriteLine("Nokia");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Telegram");
+Console.WriteLine();
+Console.WriteLine("Iphone");
+Iphone iph1 = new Iphone(numero: "789456", modelo: "Iphone", imei: "3333", memoria: 4096);
+iph1.Ligar();
+iph1.ReceberLigacao();
+iph1.InstalarAplicativo("Facebook");
