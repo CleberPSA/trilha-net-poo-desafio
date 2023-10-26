7de8990058aad177768503cd16fcf9962a6576a3
cleberPSA <cleberpacheco.araujo@gmail.com>
2023-10-26 11:38:28 -0300

    Apagando os comentarios

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,6 +1,6 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
+    
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : 
@@ -8,7 +8,7 @@ namespace DesafioPOO.Models
         {
         }
 
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+       
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"O celular Nokia está instalano o aplicativo {nomeApp}");
